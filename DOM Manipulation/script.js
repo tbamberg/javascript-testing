45260ad6825f1393611d368ff578ceb1752248d6
Tyler Bamberg <7506119+tbamberg@users.noreply.github.com>
2023-10-23 23:26:52 -0700

    Add paragraph with red text

--- DOM Manipulation/script.js
+++ DOM Manipulation/script.js
@@ -5,3 +5,10 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+const redP = document.createElement('p');
+redP.classList.add('redP');
+redP.textContent = "Hey I'm red!";
+redP.style.color = "red";
+
+container.appendChild(redP);

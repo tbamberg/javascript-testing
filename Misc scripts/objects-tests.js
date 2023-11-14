d789ebe006c4a2dd3d7cadf489d391e7acd1bf34
Tyler Bamberg <7506119+tbamberg@users.noreply.github.com>
2023-11-13 23:44:07 -0800

    Add tables practice and normalizeCSS package

---
+++ Misc scripts/objects-tests.js
@@ -0,0 +1,14 @@
+/* let user = {};
+
+user.name = "John";
+user.surname = "Smith";
+alert(user.name + " " + user.surname);
+
+user.name = "Pete";
+alert(user.name + " " + user.surname);
+
+delete user.name;
+alert(user.name + " " + user.surname);
+
+*/
+
